bbcefada7e4358c89f0c7b6cf5de86b2ab32bbf9
Keyor <4016707+superkeyor@users.noreply.github.com>
2025-02-11 14:28:41 -0600

    Update weekly.py

--- rsshub/spiders/fidelity/weekly.py
+++ rsshub/spiders/fidelity/weekly.py
@@ -1,1 +1,46 @@
+import re
+import json
+import requests
+from datetime import datetime
+from rsshub.utils import DEFAULT_HEADERS
 
+domain = 'https://www.fidelity.com'
+
+def parse_news(news):
+    title = news.get('content', '') if news.get('title', '')=='' else news.get('title', '')
+
+    content = news.get('content', '')
+    detail_url = news.get('detailUrl', '')
+    time = datetime.utcfromtimestamp(int(news['time'])).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    item = {
+        'title': title,
+        'description': content,
+        'link': detail_url,
+        'pubDate': time
+    }
+    
+    return item
+
+def ctx(lang=''):
+    url = f"{domain}/learning-center/trading-investing/weekly-market-update"
+    response = requests.get(url, headers=DEFAULT_HEADERS)
+    data = response.json()
+
+    # 检查数据是否有效
+    if data['code'] != 0 or not data['data']['data']['news']:
+        return Response("No data available", mimetype='text/plain')
+
+    news_list = data.get('data', {}).get('data', {}).get('news', [])
+    print(news_list)
+
+    # 使用 parse_gobbet 解析每一条新闻
+    items = [parse_news(news) for news in news_list]
+
+    return {
+        'title': 'Futunn Live News',
+        'link': url,
+        'description': 'Futunn Live News',
+        'author': 'hillerliao',
+        'items': items
+    }
