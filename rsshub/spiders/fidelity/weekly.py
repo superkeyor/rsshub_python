342ce4a1415aa938985301b25539b8474fbd19db
Keyor <4016707+superkeyor@users.noreply.github.com>
2025-02-11 16:28:44 -0600

    Update weekly.py

--- rsshub/spiders/fidelity/weekly.py
+++ rsshub/spiders/fidelity/weekly.py
@@ -3,7 +3,7 @@ import json
 import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
-from rsshub.utils import DEFAULT_HEADERS, fetch, fetch_by_puppeteer
+from rsshub.utils import DEFAULT_HEADERS, fetch, fetch_by_puppeteer, escape_html
 
 domain = 'https://www.fidelity.com'
 
@@ -20,20 +20,7 @@ def parse_news(news):
         'link': detail_url,
         'pubDate': time
     }
-    
     return item
-
-def escape_unescaped_html(html_content):
-    """
-    Escape unescaped HTML tags while preserving already escaped characters.
-    """
-    import html
-    html_content = str(html_content)
-    # Regex to match unescaped HTML tags
-    pattern = re.compile(r"(?<!&lt;)(<[^>]+>)(?!&gt;)")
-    # Replace unescaped tags with their escaped versions
-    escaped_content = pattern.sub(lambda match: html.escape(match.group(0)), html_content)
-    return escaped_content
     
 def ctx(lang=''):
     url = f"{domain}/learning-center/trading-investing/weekly-market-update"
@@ -42,7 +29,7 @@ def ctx(lang=''):
 
     item = {}
     item['title'] = soup.find('h1').text
-    item['content'] = escape_unescaped_html( soup.find('div',attrs={'id':'article-template-body'}) )
+    item['content'] = escape_html( soup.find('div',attrs={'id':'article-template-body'}) )
     item['link'] = url
     item['pubDate'] = datetime.strptime(soup.find('div',attrs={'class':'article-teaser-paragraph'}).text.split(':')[1].strip(), "%B %d, %Y")
 
