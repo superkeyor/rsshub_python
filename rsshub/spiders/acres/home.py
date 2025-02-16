eb8ae6fc6ad02eece8e6fb8f06c989195a5534b3
Keyor <jerryzhujian9@gmail.com>
2025-02-16 01:43:15 -0600

    update

--- rsshub/spiders/acres/home.py
+++ rsshub/spiders/acres/home.py
@@ -52,7 +52,7 @@ def ctx(category=''):
                     items.append(item)
     
     items = list({tuple(d.items()): d for d in items}.values())  # unique list while preserving order
-
+    print(items)
     return {
         'title': '一亩三分地',
         'link': domain,
