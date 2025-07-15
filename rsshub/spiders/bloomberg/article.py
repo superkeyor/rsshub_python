3ee8903e3b4cbf9728728d13f0195130f9655037
Keyor <4016707+superkeyor@users.noreply.github.com>
2025-07-15 11:55:45 -0500

    Update article.py

--- rsshub/spiders/bloomberg/article.py
+++ rsshub/spiders/bloomberg/article.py
@@ -16,7 +16,7 @@ def ctx(category=''):
     }
     url = routes[category]
 
-    soup, source, link, title = fetch_by_browser(url, wait=30)
+    soup, source, link, title = fetch_by_browser(url, wait=60)
     item = {}
     item['title'] = soup.find('h1').text
     abstract = soup.find('ul',class_=re.compile("abstract"))
